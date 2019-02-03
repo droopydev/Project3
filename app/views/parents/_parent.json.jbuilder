f904b2559c46fc2fc8358dea0621c2e312404a8b
Lydia Cheung <lydiacheung@Lydias-MacBook-Air.local>
2019-02-03 19:35:38 +0800

    parents tables

---
+++ app/views/parents/_parent.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! parent, :id, :saluatation, :name, :contact_no, :created_at, :updated_at
+json.url parent_url(parent, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_02_224904) do
+ActiveRecord::Schema.define(version: 2019_02_03_101535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "parents", force: :cascade do |t|
+    t.string "saluatation"
+    t.string "name"
+    t.integer "contact_no"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tuitions", force: :cascade do |t|
     t.string "company_id"
