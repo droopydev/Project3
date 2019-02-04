dbbe7c25a16ddffdb4914d9d756a1b243d9aa9fd
Serene Lim <Serene@serenes-mac.local>
2019-02-04 11:25:21 +0800

    added devise restriction logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   # devise_scope :user do
-  # root 'devise/sessions#new'
   # #   get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
   #   get "/users/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
   # end
 
   devise_for :users
+  root 'tuitions#index'
   get '/tuitions' => 'tuitions#index', as: 'tuitions'
   get '/tuitions/new' => 'tuitions#new', as: 'new_tuition'
   post '/tuitions' => 'tuitions#create'
