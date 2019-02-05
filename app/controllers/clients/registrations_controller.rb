91320a3bfee7bd5b471ffdeac69768b0ec95f0ba
Lydia Cheung <lydiacheung@Lydias-MacBook-Air.local>
2019-02-05 14:55:51 +0800

    devise + modal

--- app/controllers/clients/registrations_controller.rb
+++ app/controllers/clients/registrations_controller.rb
@@ -52,7 +52,8 @@ class Clients::RegistrationsController < Devise::RegistrationsController
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
-  #   super(resource)
+    # super(resource)
+    # parents_path
   # end
 
   # The path used after sign up for inactive accounts.
