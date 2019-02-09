179e47a564096a0ad2cf1ab09a6ec41280984357
minimicrowave <lingserene12@gmail.com>
2019-02-09 16:23:49 +0800

    edited devise error message appearance-dom

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
+    def resource_name
+        @resource_name ||= if client_controller?
+          :client
+        else
+          :user
+        end
+      end
       
+      def resource
+        @resource ||= resource_name.to_s.classify.constantize.new
+      end
+      
+      def devise_mapping
+        @devise_mapping ||= Devise.mappings[resource_name]
+      end
 end
