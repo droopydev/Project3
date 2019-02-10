12d07dd0184dc2fafd9b2e1eb61011900f1bbb74
minimicrowave <lingserene12@gmail.com>
2019-02-10 14:52:00 +0800

    - added restrictions to parents controllers - added db references and 1-1 r/s betwen client & parent - added new route / changed base route of parent login

--- db/migrate/20190203101535_create_parents.rb
+++ db/migrate/20190203101535_create_parents.rb
@@ -4,6 +4,7 @@ class CreateParents < ActiveRecord::Migration[5.2]
       t.string :salutation
       t.string :name
       t.integer :contact_no
+      t.references :client
 
       t.timestamps
     end
