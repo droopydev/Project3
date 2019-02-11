bc368d72816b8c291312b8f3a3f78088b0ceafe8
minimicrowave <lingserene12@gmail.com>
2019-02-12 02:59:35 +0800

    debugged courses filter

--- app/assets/javascripts/course.js
+++ app/assets/javascripts/course.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     // remove devise_error_message default formatting
     document.getElementById("filtercatbtn").addEventListener('click', openForm);
     document.getElementById("filteragebtn").addEventListener('click', openForm);
@@ -9,25 +9,25 @@ window.addEventListener("load", function() {
 
 
 function openForm() {
-   this.nextElementSibling.style.display = "block";
-   this.removeEventListener('click', openForm);
-   this.addEventListener('click', closeForm);
- }
- 
- function closeForm() {
-   this.nextElementSibling.style.display = "none";
-   this.removeEventListener('click', closeForm);
-   this.addEventListener('click', openForm);
- }
+    this.nextElementSibling.style.display = "block";
+    this.removeEventListener('click', openForm);
+    this.addEventListener('click', closeForm);
+}
 
- function reset() {
+function closeForm() {
+    this.nextElementSibling.style.display = "none";
+    this.removeEventListener('click', closeForm);
+    this.addEventListener('click', openForm);
+}
+
+function reset() {
     var allCards = document.querySelectorAll(".card")
-    allCards.forEach(function(eachCard){
+    allCards.forEach(function (eachCard) {
         eachCard.style.display = "";
     })
- }
+}
 
- function scanForm() {
+function scanForm() {
     var query;
     var values = [];
     if (this.id === "filteragesubmit") {
@@ -36,24 +36,30 @@ function openForm() {
         query = ".filtercat";
     }
 
-    document.querySelectorAll(query).forEach(function(eachCheckbox){
+    document.querySelectorAll(query).forEach(function (eachCheckbox) {
         if (eachCheckbox.checked) {
             values.push(eachCheckbox.value);
-        }    
+        }
     })
-    
-    if (values.length  > 0) {
-        filterDisplay(values);
+
+    if (values.length > 0) {
+        filterDisplay(values, query);
     }
 
-    function filterDisplay(array) {
-        console.log(array);
-        var allCards = document.querySelectorAll(".card")
-        allCards.forEach(function(eachCard){
-            if (!array.includes(eachCard.children[1].children[1].children[0].textContent.replace(/\s/g,''))){
+}
+
+function filterDisplay(array, query) {
+    console.log(array);
+    var allCards = document.querySelectorAll(".card")
+    allCards.forEach(function (eachCard) {
+        if (query === ".filterage"){
+            if (!array.includes(eachCard.children[1].children[1].children[0].textContent)) {
                 eachCard.style.display = "none";
-            } 
-        })
-    }
-
- }
+            }
+        } else {
+            if (!array.includes(eachCard.children[1].children[1].children[1].textContent.replace(/\s/g,''))) {
+                eachCard.style.display = "none";
+            }
+        }
+    })
+}
